a5f5610264bf05d9def242a24639aa97d8b28ee8
Santiago Restrepo <santiago.restrepo@globoavans.com>
2017-05-18 09:13:12 -0500

    Modified the laravel guide

--- formularios/routes/web.php
+++ formularios/routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 
 Route::get('formularios/formularios_usuario', 'FormularioController@formularios_usuario');
 Route::resource('empleado', 'EmpleadoController');
-Route::resource('formularios', 'FormularioController');
+
 Route::resource('/', 'InicioController');
 Route::post('autenticar', 'LogueoController@authenticate');
 Route::get('ingreso', 'LogueoController@index');
